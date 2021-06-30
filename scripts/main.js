2a1e28bb458fec2645f01a64c109d849c3c9fa0d
Liridona Isufi <isufi.liridona.dev@gmail.com>
2021-06-30 11:38:34 +0200

    add structure of the site

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,9 @@ class MyHeader extends HTMLElement {
                     <ul>
                         <li><a href="#">Home</a></li>
                         <li><a href="#">About</a></li>
-                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Legal Notice</a></li>
+                        <li><a href="#">E-commerce</a></li>
+                        <li><a href="#">Idea</a></li>
                     </ul>
                 </nav>
             </header>
