0c657d4e176fc382fc8787854b89f667ce6c6e3e
Liridona Isufi <isufi.liridona.dev@gmail.com>
2021-06-30 16:51:13 +0200

    add video

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,9 @@
-class MyHeader extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
-            <header>
-                <a href="index.html">
-                    <img src="../images/pagehaut.png" alt="">
-                </a>
-
-                <div class"container">
-                    <nav class="nav-bar">
-                        <ul>
-                            <li><a href="../index.html">Home</a></li>
-                            <li><a href="pages/about.html">About</a></li>
-                            <li><a href="pages/legale-notice.html">Legal Notice</a></li>
-                            <li><a href="#">E-commerce</a></li>
-                            <li><a href="#">Idea</a></li>
-                        </ul>
-                    </nav>
-                <div>
-            </header>
-        `
-    }
-}
-
-customElements.define('my-header', MyHeader)
 
 class MyFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <footer>
-                &copy; Copyright 2021 Team Name
+                &copy; Copyright 2021 Hackathon 01
             </footer>
         `
     }
